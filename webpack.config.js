954b4dbd88036bc7c33bc21877e6c09389f88563
dinhlongnguyen <dinhlongviolin1@gmail.com>
2023-01-12 21:35:29 +0700

    add license (#2)
    * add license
    
    * update 2023

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2023 Avaiga Private Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 //@ts-check
 
 'use strict';
@@ -11,7 +24,7 @@ const copyPlugin = require("copy-webpack-plugin");
 /** @type WebpackConfig */
 const extensionConfig = {
   target: 'node', // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
-	mode: 'development', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+    mode: 'development', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 
   entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
   output: {
