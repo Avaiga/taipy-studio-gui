9d8f4c307464ffb94823368025831971f32af752
dinhlongnguyen <dinhlongviolin1@gmail.com>
2023-01-31 20:38:02 +0700

    crlf

--- src/extension.ts
+++ src/extension.ts
@@ -14,7 +14,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import { GuiContext } from "./gui";
-import { ExtensionContext} from "vscode";
+import { ExtensionContext } from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
