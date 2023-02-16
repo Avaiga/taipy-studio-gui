d05d69d6acb2c1d9d32486d7e08d406383ed609e
dinhlongnguyen <dinhlongviolin1@gmail.com>
2023-02-16 15:06:59 +0700

    markdownit template

--- src/extension.ts
+++ src/extension.ts
@@ -13,13 +13,20 @@
 
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { GuiContext } from "./gui";
 import { ExtensionContext } from "vscode";
+import type MarkdownIt from 'markdown-it';
+import { GuiContext } from "./gui";
+import { markdownItTaipyStudioGui } from "./markdownIt";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export async function activate(context: ExtensionContext) {
     GuiContext.register(context);
+    return {
+        extendMarkdownIt(md: MarkdownIt) {
+            return md.use(markdownItTaipyStudioGui);
+        },
+    };
 }
 
 // This method is called when your extension is deactivated
