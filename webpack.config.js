1117b5a7cb8b1385c9869e598cd24f99802e1c3e
dinhlongnguyen <dinhlongviolin1@gmail.com>
2023-02-16 14:36:29 +0700

    add preview scripts

--- webpack.config.js
+++ webpack.config.js
@@ -13,9 +13,9 @@
 
 //@ts-check
 
-'use strict';
+"use strict";
 
-const path = require('path');
+const path = require("path");
 const copyPlugin = require("copy-webpack-plugin");
 
 //@ts-check
@@ -23,58 +23,68 @@ const copyPlugin = require("copy-webpack-plugin");
 
 /** @type WebpackConfig */
 const extensionConfig = {
-  target: 'node', // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
-    mode: 'development', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+    target: "node", // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+    mode: "development", // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 
-  entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
-  output: {
-    // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'taipy-studio-gui.js',
-    libraryTarget: 'commonjs2',
-    clean: true,
-  },
-  externals: {
-    vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
-    // modules added here also need to be added in the .vscodeignore file
-  },
-  resolve: {
-    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-    extensions: ['.ts', '.js']
-  },
-  plugins: [
-    new copyPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, "l10n"),
-          to: "l10n",
-        },
-        {
-          from: path.resolve(__dirname, "snippets.json"),
-        },
-        {
-          from: path.resolve(__dirname, "src", "assets"),
-          to: "assets",
-        },
-      ]
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'ts-loader'
-          }
-        ]
-      },
+    entry: "./src/extension.ts", // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+    output: {
+        // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+        path: path.resolve(__dirname, "dist"),
+        filename: "taipy-studio-gui.js",
+        libraryTarget: "commonjs2",
+    },
+    externals: {
+        vscode: "commonjs vscode", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+        // modules added here also need to be added in the .vscodeignore file
+    },
+    resolve: {
+        // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+        extensions: [".ts", ".js"],
+    },
+    plugins: [
+        new copyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "l10n"),
+                    to: "l10n",
+                },
+                {
+                    from: path.resolve(__dirname, "snippets.json"),
+                },
+                {
+                    from: path.resolve(__dirname, "src", "assets"),
+                    to: "assets",
+                },
+            ],
+        }),
     ],
-  },
-  devtool: 'nosources-source-map',
-  infrastructureLogging: {
-    level: "log", // enables logging required for problem matchers
-  },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader",
+                    },
+                ],
+            },
+        ],
+    },
+    devtool: "nosources-source-map",
+    infrastructureLogging: {
+        level: "log", // enables logging required for problem matchers
+    },
 };
-module.exports = [ extensionConfig ];
+
+const previewConfig = {
+    ...extensionConfig,
+    entry: "./markdownPreview/index.ts",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "taipy-studio-gui-markdown-preview.js",
+        libraryTarget: "commonjs2",
+    },
+};
+
+module.exports = [extensionConfig, previewConfig];
